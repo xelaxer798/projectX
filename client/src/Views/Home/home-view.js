6a25f3fb80bc9f67ac9f717b47636c9009c65b71
David Horn <david.horn689@gmail.com>
2018-07-28 18:14:27 -0700

    changed test sign in

--- client/src/Views/Home/home-view.js
+++ client/src/Views/Home/home-view.js
@@ -5,7 +5,7 @@ import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 class Home extends Component {
   state={
-    email: 'david.horn689@gmail.com',
+    email: 'jeff@gmail.com',
             password: 'projectgreen',
             doesntMatch: false,
             noUser: false
