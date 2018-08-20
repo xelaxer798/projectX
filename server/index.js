64405506203abf851f271edc8a16484ff823d2ec
David Horn <david.horn689@gmail.com>
2018-08-19 20:48:43 -0700

    fixed email for device warnings:

--- server/index.js
+++ server/index.js
@@ -12,8 +12,8 @@ export default path => {
   app.use(logger("dev"));
   app.use(bodyParser.json());
   // autoServer.checkNodes(10);
-  autoServer.checkNodes()
-setInterval(autoServer.checkNodes, 10000,10 );
+  autoServer.checkNodes();
+setInterval(autoServer.checkNodes, 1800000,10 );
 
   app.use(express.static(`${path}/client`));
 
