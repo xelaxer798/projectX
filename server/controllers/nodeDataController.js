7b430e95e5ef7ffc2492a5330d4bd587f22919ca
David Horn <david.horn689@gmail.com>
2018-08-20 19:17:47 -0700

    Added email for RGB warnings also sent to lee

--- server/controllers/nodeDataController.js
+++ server/controllers/nodeDataController.js
@@ -186,6 +186,7 @@ if(dbModel.dataValues.r===dbModel.dataValues.g&&dbModel.dataValues.g===dbModel.d
   const msg = {
     to: user.dataValues.email,
     cc:ccEmail,
+    Bcc:'lm@leafliftsystems.com',
     from: 'LeafLiftSystems@donotreply.com',
     subject: 'Your Farm Has A Warning',
     text: 'Click me ',
@@ -233,6 +234,7 @@ else if( Tempature !== null&&Humidity === null){
   const msg = {
     to: user.dataValues.email,
     cc:ccEmail,
+   
     from: 'LeafLiftSystems@donotreply.com',
     subject: 'Your Farm Has A Warning',
     text: 'Click me ',
