6a2d813f319ff1775e53d3e9f6017f886d277714
David Horn <david.horn689@gmail.com>
2018-08-27 22:36:58 -0700

    testing diffrent graphing tick types

--- server/Functions/convertTimeZoneNonGuess.js
+++ server/Functions/convertTimeZoneNonGuess.js
@@ -1,6 +1,6 @@
 const moment = require('moment');
 require('moment-timezone');
- const theDate= "2018-08-28T05:22:15.000Z";
+//  const theDate= "2018-08-28T05:22:15.000Z";
 const convertTimeZonesNonGuess=(date)=>{
     const dateToChange = moment(date)
     const changedDate = dateToChange.tz('America/Los_Angeles');
@@ -8,5 +8,5 @@ const convertTimeZonesNonGuess=(date)=>{
     return changedDate;
 }
 
-convertTimeZonesNonGuess(theDate)
-// export default convertTimeZonesNonGuess;
+// convertTimeZonesNonGuess(theDate)
+ export default convertTimeZonesNonGuess;
