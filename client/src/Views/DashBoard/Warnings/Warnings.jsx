8291182d10539def708a1e9e6d47dffa8f6cbec8
David Horn <david.horn689@gmail.com>
2018-09-03 12:40:10 -0700

    added a grid to dashboard

--- client/src/Views/DashBoard/Warnings/Warnings.jsx
+++ client/src/Views/DashBoard/Warnings/Warnings.jsx
@@ -8,7 +8,11 @@ class Warnings extends Component {
     humidityWarnings: [],
     RGBWarnings: [],
     deviceWarnings: [],
-    loading: true
+    loading: true,
+    tempLength: 0,
+    humidLength: 0,
+    deviceLength: 0,
+
   };
   componentDidCatch = (error, info) => {
     console.log('hi i am catching warnings');
@@ -22,60 +26,82 @@ class Warnings extends Component {
   };
   getWarnings = async () => {
     let warnings = await WarningsApi.getWarnings(this.props.userid);
-console.log(warnings.data.tempature.length)
-if(warnings.data.tempature.length<1&&warnings.data.humidity.length <1&&warnings.data.device.length<1){
-  this.setState({
-    tempatureWarnings: [{num:0,warning:'no warnings'}],
-    humidityWarnings: [{num:0,warning:'no warnings'}],
-    deviceWarnings: [{num:0,warning:'no warnings'}],
-    loading: false
-  });
-}else{
-  this.setState({
-    tempatureWarnings: warnings.data.tempature,
-    humidityWarnings: warnings.data.humidity,
-    deviceWarnings: warnings.data.device,
-    loading: false
-  });
-}
-   
+    if (warnings.data.tempature.length < 1 && warnings.data.humidity.length < 1 && warnings.data.device.length < 1) {
+      this.setState({
+        tempLengt: 0,
+        humidLength: 0,
+        deviceLength: 0,
+        loading: false
+      });
+    } else {
+      this.setState({
+        tempLength: warnings.data.tempature.length,
+        humidLength: warnings.data.humidity.length,
+        deviceLength: warnings.data.device.length,
+        tempatureWarnings: warnings.data.tempature,
+        humidityWarnings: warnings.data.humidity,
+        deviceWarnings: warnings.data.device,
+        loading: false
+      });
+    }
+
   };
   render() {
+    let tempatureWarnings = null;
+    let humidityWarnings = null;
+    let deviceWarnings = null;
+
+    if (this.state.tempLength < 1) {
+      tempatureWarnings = <div></div>
+    }
+    else {
+      tempatureWarnings =    this.state.tempatureWarnings.map((tile) => (
+        <div key={tile.num}>
+          <li >{tile.warning} </li>
+          <li >{tile.time}</li>
+        </div>
+      ))
+    }
+    if (this.state.humidLength < 1) {
+      humidityWarnings  = <div></div>
+    }
+    else {
+      humidityWarnings = this.state.humidityWarnings.map((tile) => (
+        <div key={tile.num}>
+          <li >{tile.warning} </li>
+          <li >{tile.time}</li>
+        </div>
+      ));
+    };
+
+    //Device Warnings
+    if (this.state.deviceLength < 1) {
+      deviceWarnings = <div></div>
+    }
+    else {
+      deviceWarnings= this.state.deviceWarnings.map((tile) => (
+        <div key={tile.num}>
+          <li >{tile.warning} </li>
+          <li >{tile.time}</li>
+        </div>
+      ));
+    };
     return (
       <div>
         {!this.state.loading ? <Grid container spacing={16}> <Grid item xs={3}>
 
           <h3>Tempature Warnings</h3>
-          {this.state.tempatureWarnings.map((tile) => (
-            <div key={tile.num}>
-              <li >{tile.warning} </li>
-              <li >{tile.time}</li>
-            </div>
-
-
-          ))}
+          {tempatureWarnings}
         </Grid>
           <Grid item xs={3}>
             <h3>Humidity Warnings</h3>
-            {this.state.humidityWarnings.map((tile) => (
-              <div key={tile.num}>
-                <li >{tile.warning} </li>
-                <li >{tile.time}</li>
-              </div>
-
-
-            ))}
+            {humidityWarnings}
           </Grid>
 
           <Grid item xs={3}>
             <h3>Device Warnings</h3>
 
-            {this.state.deviceWarnings.map((tile) => (
-              <div key={tile.num}>
-                <li >{tile.warning}</li>
-                <li >{tile.time}</li>
-              </div>
-            ))}
+            {deviceWarnings}
           </Grid></Grid> : <div>
             <h1>Your Warnings are loading</h1>
             <img src={Images.loadingGif} alt='loading' />
