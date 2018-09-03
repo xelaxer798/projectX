8532a29da5f49edda78eef9eba916026d8dff031
David Horn <david.horn689@gmail.com>
2018-09-03 00:59:55 -0700

    test

--- server/Functions/getFormatedTime.js
+++ server/Functions/getFormatedTime.js
@@ -4,7 +4,7 @@ require('moment-timezone');
 
 const getFormateTime = (date) => {
         const month = functions.getMonth(date);
-        const time = moment(date).tz("America/Los_Angeles").format('YYYY-MM-DD hh:mm:ss a').split(' ');
+        const time = moment(date).tz("America/Los_Angeles").format('YYYY-MM-DD hh:mm a').split(' ');
         const splitDate = time[0].split('-');
         const timeString = `${month} ${splitDate[2]} ${splitDate[0]}. ${time[1]} ${time[2]}`;
         return timeString;
