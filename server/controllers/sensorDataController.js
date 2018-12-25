c657448013b7734a64bb275017ecb1469938fc12
Lee Mandell <lm@leafliftsystems.com>
2018-12-24 22:17:16 -0800

    Storing current value for sensors when they post data

--- server/controllers/sensorDataController.js
+++ server/controllers/sensorDataController.js
@@ -1,5 +1,6 @@
 import db from "../models";
 import functions from "../Functions";
+import SensorDataAPI from "../../client/src/Data/sensorData-api";
 
 const controller = {
     findBySensorId: function (req, res) {
@@ -62,15 +63,75 @@ const controller = {
             {where: {
                 nodeId: req.body.nodeId
                 }}
-        )
+         )
             .then(dbModel => {
                 console.log("zzJSON input update node: " + JSON.stringify(dbModel))
+                return null;
             })
             .catch(err => {
                 console.log("Error: " + err);
+            });
+        let promises = req.body.sensorData.map(function (individualSensorData) {
+            console.log("Create promise: " + individualSensorData.sensorId);
+            // return SensorDataAPI.getAll(selectedGraphs.sensorId);
+            return db.Sensors.update(
+                {currentValue: individualSensorData.dataValueFloat},
+                {where: {
+                        sensorId: individualSensorData.sensorId
+                    }}
+            )
+        });
+
+        Promise.all(promises).then(() =>{
+            console.log("Uddating sensor: " + req.body.sensorId + " to " + req.body.dataValueFloat)
+            return null;
+        })
+
+//         db.Sensors.update(
+//             {currentValue: req.body.dataValueFloat},
+//             {where: {
+//                 sensorId: req.body.sensorId
+//                 }}
+//         )
+//             .then(() =>{
+//                 console.log("Uddating sensor: " + req.body.sensorId + " to " + req.body.dataValueFloat)
+// return null;
+//             })
+//             .catch(err => {
+//                 console.log("Error: " + err);
+//             })
+    },
+    create2: function (req, res) {
+        const request = req;
+        db.SensorData.bulkCreate(req.body.sensorData)
+            .then (() => {
+                db.Nodes.update(
+                    {lastUpdate: new Date()},
+                    {where: {
+                            nodeId: req.body.nodeId
+                        }}
+                )
+                    .then(() => {
+                        console.log("Uddating sensor: " + request.body.sensorId + " to " + request.body.dataValueFloat)
+                        db.Sensors.update(
+                            {currentValue: request.body.dataValueFloat},
+                            {where: {
+                                    sensorId: request.body.sensorId
+                                }}
+                        )
+
+                    })
+                    .catch(err => {
+                        console.log("Error: " + err);
+                        res.status(422).json(err)
+                    })
+            })
+             .catch(err => {
+                console.log("Error: " + err);
                 res.status(422).json(err)
             });
-    },
+
+    }
     // create: function (req, res) {
     //     console.log("JSON input: " + req.body);
     //     db.SensorData.create({
