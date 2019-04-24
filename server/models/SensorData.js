d97f5f181c0699d978494ab856820c1d658f6f54
Lee Mandell <lm@leafliftsystems.com>
2019-04-24 16:19:09 -0700

    Add endTime to sensorData

--- server/models/SensorData.js
+++ server/models/SensorData.js
@@ -18,6 +18,10 @@ module.exports = function (sequelize, DataTypes) {
             type: DataTypes.STRING,
             allowNull: true
         },
+        endTime: {
+            type: DataTypes.DATETIME,
+            allowNull: true
+        }
 
     }, {
         indexes: [
